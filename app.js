fa9034d98363eb42ffb92b1348adc7e01f8a209a
joshuva george <joshuva@adaptivemedia.in>
2019-03-27 11:03:52 +0530

    added basic layout for website

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-(function() {
-    document.addEventListener('DOMContentLoaded', addListener);
-})
+// (function() {
+//     document.addEventListener('DOMContentLoaded', addListener);
+// }());
 
-function addListener() {
-    var hex = document.querySelector('#hexCode');
-    var rgb = document.querySelector('#rgbValue');
-    hex.addEventListener('input', (ev) => {
-        console.log('input from hex code', ev.type, ev.currentTarget, ev.target);
-    }, false);
-    rgb.addEventListener('input', (ev) => {
-        console.log('input from rgb value', ev.type, ev.currentTarget);
-    });
-}
+// function addListener() {
+//     var hex = document.querySelector('#hexCode');
+//     var rgb = document.querySelector('#rgbValue');
+//     hex.addEventListener('input', (ev) => {
+//         console.log('input from hex code', ev.type, ev.currentTarget, ev.target);
+//     }, false);
+//     rgb.addEventListener('input', (ev) => {
+//         console.log('input from rgb value', ev.type, ev.currentTarget);
+//     });
+// }
